03639990442bcb45b6961ae4f08bcf7a692db230
SPADTCC <psingh9@dtcc.edu>
2019-08-09 17:50:22 -0400

    longest palindrome

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,39 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+    // expandAroundMiddle
+    let startIndex = 0;
+    let maxLength = 1;  // since single char string is a palindrome
+
+    let midpoint = Math.floor(s.length) / 2;
+
+    const expandAroundMiddle = (left, right) => {
+
+        while (s[left] === s[right] && left >= 0 && right < s.length) {
+            const currentPalindromeLength = right - left + 1;
+            if (currentPalindromeLength > maxLength) {
+                maxLength = currentPalindromeLength;
+                startIndex = left;
+            }
+
+            left--;
+            right++
+        }
+
+    }
+
+
+    // loop all elements and expandAroundMiddle
+    for (let i = 0; i < s.length; i++) {
+        expandAroundMiddle(i - 1, i + 1);
+        expandAroundMiddle(i, i + 1);
+    }
+
+
+    return s.slice(startIndex, startIndex + maxLength);
+
+
+}
 
 module.exports = longestPalindrome;
